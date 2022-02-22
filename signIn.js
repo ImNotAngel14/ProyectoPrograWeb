c58bdf17b61e6f6b03fca90ef3ef7e8550b3d804
unknown <barbosamane@hotmail.com>
2022-02-22 00:53:10 -0600

    Sign in terminado y logos agregados

--- signIn.js
+++ signIn.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $("#guardarRegistro").click(function(){
-        <a href="login.html" target="_blank"> Link </a>
         //window.open("login.html", "A", "width=300, height=200")
         //alert("Guardado con exito");
     });
