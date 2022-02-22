111634dd0547b8376fb8e02cd3e1413392355555
unknown <barbosamane@hotmail.com>
2022-02-21 22:23:50 -0600

    Finalize la pagina de registro

---
+++ signIn.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#guardarRegistro").click(function(){
+        <a href="login.html" target="_blank"> Link </a>
+        //window.open("login.html", "A", "width=300, height=200")
+        //alert("Guardado con exito");
+    });
+    var input = $( "input:image" ).css({
+        background:"yellow",
+        border:"3px red solid"
+      });
+});
